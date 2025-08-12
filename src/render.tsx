5cfbb45fb610dbb97ef5994a5719d3b786e44ef5
Elietio <xzc549212830@gmail.com>
2025-08-12 20:16:04 +0800

    refactor(src): 将认证标签从 Auth Key 改为 ACCESS_KEY

--- src/render.tsx
+++ src/render.tsx
@@ -15,7 +15,7 @@ export const Render = ({ isAuthenticated, showWarning }: { isAuthenticated: bool
 						<form id="login-form" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
 							<div class="mb-4">
 								<label class="block text-gray-700 text-sm font-bold mb-2" for="auth-key">
-									Auth Key
+									ACCESS_KEY
 								</label>
 								<input
 									class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
